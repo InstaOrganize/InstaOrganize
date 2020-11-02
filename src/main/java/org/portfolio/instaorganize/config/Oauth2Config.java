011d437ce2f4f0928428d82ec0c0f96472a835af
prsquared <priyesh321988@gmail.com>
2020-11-01 23:37:49 -0500

    altered config for h2-console

--- src/main/java/org/portfolio/instaorganize/config/Oauth2Config.java
+++ src/main/java/org/portfolio/instaorganize/config/Oauth2Config.java
@@ -8,7 +8,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
 
 @Configuration
 public class Oauth2Config extends WebSecurityConfigurerAdapter {
@@ -18,20 +22,35 @@ public class Oauth2Config extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(a -> a
-                .antMatchers("/", "/error", "/webjars/**").permitAll()
+                .antMatchers("/", "/h2-console/**", "/error", "/webjars/**").permitAll()
                 .anyRequest().authenticated())
                 .logout(l -> l
                         .logoutSuccessUrl("/").permitAll())
-                .csrf(c -> c
+                /*.csrf(c -> c
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                )
-                .exceptionHandling(e -> e
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                )*/
+                .csrf().disable()
+                .headers().frameOptions().disable();
+        http.exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .oauth2Login(o -> o
                         .failureHandler((request, response, exception) -> {
                             request.getSession().setAttribute("error.message", exception.getMessage());
                             handler.onAuthenticationFailure(request, response, exception);
                         })
                 );
+        /*http.csrf().requireCsrfProtectionMatcher(new RequestMatcher() {
+            private Pattern allowedMethods = Pattern.compile("^ (GET | HEAD | TRACE | OPTIONS) $");
+            private RegexRequestMatcher apiMatcher = new RegexRequestMatcher("/ console /.*",null);
+
+            @Override
+            public boolean matches(HttpServletRequest request) {
+                if (allowedMethods.matcher(request.getMethod()).matches())
+                    return false;
+                if (apiMatcher.matches(request))
+                    return false;
+                return true;
+            }
+        });*/
     }
 }
